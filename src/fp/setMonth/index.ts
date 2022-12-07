7adf3612eb9fce7862c00fb18ff13ca4882456d4
Colin Young <colin.young@joinmosaic.com>
2022-12-07 08:39:21 -0600

    Move unused files

--- src/fp/setMonth/index.ts
+++
@@ -1,6 +0,0 @@
-// This file is generated automatically by `scripts/build/fp.ts`. Please, don't change it.
-
-import fn from '../../setMonth/index'
-import convertToFP from '../_lib/convertToFP/index'
-
-export default convertToFP(fn, 2)
